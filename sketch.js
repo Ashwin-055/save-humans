b4a6ab6abe4069d88c434e87a3987fde000f00ac
Ashwin <70658331+Ashwin-055@users.noreply.github.com>
2020-09-30 17:34:55 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
           friction:0
         }
 	
-	planeSound.play();
+	planeSound.loop();
         packageSprite=createSprite(width/2, 200, 10,10);	
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.15
